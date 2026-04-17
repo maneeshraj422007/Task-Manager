85eb9bc83dda34277fcb8f662903b64ce8a79222
Maneesh Raj <maneeshraj422007@gmail.com>
2026-04-17 21:37:05 +0530

    Final working version

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -6,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/taskdb")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
