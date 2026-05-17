ac4758c6884a204e1e426dfc67b372243efd15ca
Maneesh Raj <maneeshraj422007@gmail.com>
2026-05-17 15:50:10 +0530

    Task Manager with MERN Applications

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,7 +9,13 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://maneeshraj422007.github.io"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 /* ================== DATABASE ================== */
